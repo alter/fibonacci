d5197b05b9ef946ec95284e6ba62c382a412fd5b
alter <alter.pub@gmail.com>
2015-12-03 15:25:08 +0300

    MAX_RECURSION vars

--- fibonachi.py
+++ fibonachi.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 import sys
 import re
-sys.setrecursionlimit(10000)
+MAX_RECURSION = 1000
+sys.setrecursionlimit(MAX_RECURSION)
 
 def usage():
-  print "Usage: %s [seq_number]\n\tseq_number should be between 2 and 9998" % sys.argv[0]
+  print "Usage: %s [seq_number]\n\tseq_number should be between 2 and %s" % (sys.argv[0], MAX_RECURSION - 2)
   exit(1)
 
-if( len(sys.argv) < 2 or int(sys.argv[1]) < 2 or int(sys.argv[1]) > 9998 ):
+if( len(sys.argv) < 2 or int(sys.argv[1]) < 2 or int(sys.argv[1]) > MAX_RECURSION - 2 ):
   usage()
 
 i = 0
